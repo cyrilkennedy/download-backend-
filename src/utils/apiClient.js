dfd63c66f260579b9366aa10a225cec890f8f5b3
cyrilkennedy <kennedycyrilchimankpam@gmail.com>
2025-11-02 06:57:10 +0100

    x

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -2,5 +2,6 @@ export const identifyPlatform = (url) => {
   if (url.includes("tiktok.com")) return "tiktok";
   if (url.includes("instagram.com")) return "instagram";
   if (url.includes("facebook.com")) return "facebook";
+  if (url.includes("twitter.com")) return "twitter";
   return "unknown";
 };
